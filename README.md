b18ef5235edf06fb98896465e6f3c69b78fea497
chwoozy <choonweiw@gmail.com>
2022-04-14 18:17:09 -0700

    Add initial frontend and dependencies

---
+++ README.md
@@ -0,0 +1,22 @@
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version '3.0.1'
+
+* yarn
+
+* Configuration in database.yml 
+  username: <username>
+  password: <password>
+
+* rails db:create
+
+* rails db:structure:load
+
+* rails db:schema:load
+
+*  bundle exec rails db < ./db/dump.sql
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get '/dashboard/orm', to: 'dashboard#orm', as: 'orm'
+  get '/dashboard/slowdb', to: 'dashboard#slowdb', as: 'slowdb'
+  get '/dashboard/slowrequest', to: 'dashboard#slowrequest', as: 'slowrequest'
+  get '/dashboard/swallowedexception', to: 'dashboard#swallowedexception', as: 'swallowedexception'
+  get '/dashboard/untracked', to: 'dashboard#untracked', as: 'untracked'
+
+  post '/bad_app/orm', to: 'bad_app#orm', as: 'badapp_orm'
+  post '/bad_app/slowdb', to: 'bad_app#slowdb', as: 'badapp_slowdb'
+  post '/bad_app/slowrequest', to: 'bad_app#slowrequest', as: 'badapp_slowrequest'
+  post '/bad_app/swallowedexception', to: 'bad_app#swallowedexception', as: 'badapp_swallowedexception'
+  post '/bad_app/untracked', to: 'bad_app#untracked', as: 'badapp_untracked'
+  
+  root 'dashboard#index'
+end
